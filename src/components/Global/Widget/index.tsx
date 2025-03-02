019772b6fcae645685fe6c8f5bf5fa237672ac5b
Yathish G P <yathishgp1@gmail.com>
2025-03-02 20:21:31 +0530

    created widget component

---
+++ src/components/Global/Widget/index.tsx
@@ -0,0 +1,72 @@
+import { ClerkLoading, SignedIn, useUser } from "@clerk/clerk-react";
+import { Loader } from "../Loader";
+import { useEffect, useState } from "react";
+import { fetchUserProfile } from "@/lib/utils";
+import { useMediaSources } from "@/hooks/useMediaSources";
+
+const Widget = () => {
+  const [profile, setProfile] = useState<{
+    status: number;
+    user:
+      | ({
+          subscription: {
+            plan: "PRO" | "FREE";
+          } | null;
+          studio: {
+            id: string;
+            screen: string | null;
+            mic: string | null;
+            preset: "HD" | "SD";
+            camera: string | null;
+            userId: string | null;
+          } | null;
+        } & {
+          id: string;
+          email: string;
+          firstname: string | null;
+          lastname: string | null;
+          createdAt: Date;
+          clerkid: string;
+        })
+      | null;
+  } | null>(null);
+
+  const { user } = useUser();
+
+  const { state, fetchMediaResources } = useMediaSources();
+  console.log("Media state:", state);
+
+  useEffect(() => {
+    if (user && user.id) {
+      fetchUserProfile(user.id).then((p) => setProfile(p));
+    }
+  }, [user]);
+
+  // useEffect(() => {
+  //   if (profile) {
+  //     console.log("status:", profile.status);
+  //     console.log("User:", profile.user);
+  //   }
+  // }, [profile]);
+
+  return (
+    <div className="p-5">
+      <ClerkLoading>
+        <div className="h-full flex justify-center items-center">
+          <Loader />
+        </div>
+      </ClerkLoading>
+      <SignedIn>
+        {profile ? (
+          <MediaConfiguration state={state} user={profile?.user} />
+        ) : (
+          <div className="w-full h-full flex justify-center items-center">
+            <Loader color="#fff" />
+          </div>
+        )}
+      </SignedIn>
+    </div>
+  );
+};
+
+export default Widget;
