c01cdf57061413be360505450004d4636d7c7771
Yathish G P <yathishgp1@gmail.com>
2025-03-07 15:41:17 +0530

    authentication

--- src/components/Global/AuthButton/index.tsx
+++ src/components/Global/AuthButton/index.tsx
@@ -1,20 +1,20 @@
-import { SignedOut, SignInButton, SignUpButton } from "@clerk/clerk-react";
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import { SignedOut, SignInButton, SignUpButton } from '@clerk/clerk-react';
 
 const AuthButton = () => {
   return (
     <SignedOut>
-      <div className="flex gap-x-3 h-screen justify-center items-center">
+      <div className='flex gap-x-3 h-screen justify-center items-center'>
         <SignInButton>
           <Button
-            variant="outline"
-            className="px-10 rounded-full hover:bg-gray-200"
+            variant='outline'
+            className='px-10 rounded-full hover:bg-gray-200'
           >
             Sign In
           </Button>
         </SignInButton>
         <SignUpButton>
-          <Button variant="default" className="px-10 rounded-full">
+          <Button variant='default' className='px-10 rounded-full'>
             Sign Up
           </Button>
         </SignUpButton>
