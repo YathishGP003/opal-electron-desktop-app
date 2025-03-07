f501f309033c2f4fdbb48e62991179bb506d97de
Yathish G P <yathishgp1@gmail.com>
2025-03-07 15:41:50 +0530

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import path, { resolve } from "node:path";
-import electron from "vite-plugin-electron/simple";
-import tsConfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import electron from 'vite-plugin-electron/simple';
+import react from '@vitejs/plugin-react';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    emptyOutDir: false,
+    emptyOutDir: true,
     manifest: true,
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        studio_main: resolve(__dirname, 'studio.html'),
-        web_cam_main: resolve(__dirname, 'webcam.html'),
+        main: path.resolve(__dirname, 'index.html'),
+        studio: path.resolve(__dirname, 'studio.html'),
+        web_cam_main: path.resolve(__dirname, 'webcam.html'),
       },
-    },  
+    },
   },
   server: {
     proxy: {
@@ -27,25 +27,24 @@ export default defineConfig({
       },
     },
   },
-  
   plugins: [
     react(),
     tsConfigPaths(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
-        entry: "electron/main.ts",
+        entry: 'electron/main.ts',
       },
       preload: {
         // Shortcut of `build.rollupOptions.input`.
         // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, "electron/preload.ts"),
+        input: path.join(__dirname, 'electron/preload.ts'),
       },
       // Ployfill the Electron and Node.js API for Renderer process.
       // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
       // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
       renderer:
-        process.env.NODE_ENV === "test"
+        process.env.NODE_ENV === 'test'
           ? // https://github.com/electron-vite/vite-plugin-electron-renderer/issues/78#issuecomment-2053600808
             undefined
           : {},
@@ -53,7 +52,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
